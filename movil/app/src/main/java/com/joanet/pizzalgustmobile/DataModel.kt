aaf092ca1c12973cb3192545d3c73132fa21f948
J0anet <joan.enajas@gmail.com>
2024-03-15 20:07:03 +0100

    LLAMA A API PERO NO RESPONDE EL NOMBRE

---
+++ movil/app/src/main/java/com/joanet/pizzalgustmobile/DataModel.kt
@@ -0,0 +1,9 @@
+package com.joanet.pizzalgustmobile
+
+
+
+// Model class for our Jokes
+data class DataModel(
+    val nombre: String
+
+):java.io.Serializable
--- movil/app/src/main/java/com/joanet/pizzalgustmobile/MainActivity.kt
+++ movil/app/src/main/java/com/joanet/pizzalgustmobile/MainActivity.kt
@@ -1,67 +1,88 @@
 package com.joanet.pizzalgustmobile
 
-import android.os.Bundle
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.appcompat.widget.AppCompatButton
-import com.joanet.pizzalgustmobile.R.*
-import com.joanet.pizzalgustmobile.api.ApiClient
-import com.joanet.pizzalgustmobile.api.ApiService
-import com.joanet.pizzalgustmobile.api.Post
-import com.joanet.pizzalgustmobile.api.TestRequestBody
-import kotlinx.coroutines.flow.internal.NoOpContinuation.context
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.ProgressBar
+import android.widget.TextView
+import com.google.gson.Gson
+import com.google.gson.JsonObject
+import okhttp3.RequestBody
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
-import kotlin.coroutines.jvm.internal.CompletedContinuation.context
+
+
+
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var btn_jokes: Button
+    private lateinit var tv_jokes: TextView
+    private lateinit var progressBar: ProgressBar
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         setContentView(R.layout.activity_main)
 
-        val button = findViewById<AppCompatButton>(id.btn_api)
-        button.setOnClickListener {
-            fun main() {
-                val retrofit = Retrofit.Builder()
-                    .baseUrl("http://localhost:5002/")
-                    .addConverterFactory(GsonConverterFactory.create())
-                    .build()
+        // Find the button view by its ID and
+        // assign it to a variable.
+        btn_jokes = findViewById(R.id.btn_joke)
 
-                val apiService = retrofit.create(ApiService::class.java)
+        // Find the text view by its ID and
+        // assign it to a variable.
+        tv_jokes = findViewById(R.id.tv_joke)
 
-                val requestBody = TestRequestBody("Raul")
-                val contentType = "application/json"
+        // Find the progress bar and assign
+        // it to the varriable.
+        progressBar = findViewById(R.id.idLoadingPB)
 
-                val call = apiService.test(contentType, requestBody)
+        // Set an OnClickListener on the button view.
+        // Dentro de tu función onClickListener
+        btn_jokes.setOnClickListener {
+            // Mostrar la barra de progreso
+            progressBar.visibility = View.VISIBLE
 
-                call.enqueue(object : Callback<TestRequestBody> {
-                    // This is an anonymous inner class that implements the Callback interface.
-                    override fun onResponse(response: Response<TestRequestBody>?, retrofit: Retrofit?) {
-                        // This method is called when the API response is received successfully.
-
-                        if(response!!.isSuccess){
-                            // If the response is successful, parse the
-                            // response body to a DataModel object.
-                            val jokes: TestRequestBody = response.body() as TestRequestBody
-
-                            // Call the callback function with the DataModel
-                            // object as a parameter.
-                            callback(jokes)
-                        }
-                    }
-
-                    override fun onFailure(call: Call<TestRequestBody>, t: Throwable) {
-                        // This method is called when the API request fails.
-                        println("fallo")
-                    }
+            // Crear un objeto JSON
+            val jsonObject = JsonObject()
 
 
-            }
+            // Convertir el objeto JSON a una cadena utilizando Gson
+            val jsonString = Gson().toJson(jsonObject)
+
+
+
+
+            // Llamar al método getJokes() de la clase ApiCall,
+            // pasando una función de devolución de llamada como parámetro.
+            val retrofit = Retrofit.Builder()
+                .baseUrl("http://10.0.2.2:5002/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
+
+            val apiService = retrofit.create(ApiService::class.java)
+            apiService.getJokes().enqueue(object : Callback<DataModel> {
+                override fun onResponse(call: Call<DataModel>, response: Response<DataModel>) {
+                    // Establecer el texto del TextView con el
+                    // valor de la broma devuelta por la respuesta de la API.
+                    tv_jokes.setText(response.body()?.nombre ?: "No se encontraron Nombres")
+
+
+                    // Ocultar la barra de progreso
+                    progressBar.visibility = View.GONE
+                }
+
+                override fun onFailure(call: Call<DataModel>, t: Throwable) {
+                    // Manejar la falla de la solicitud aquí
+                    Log.e("API_CALL", "Error: ${t.message}")
+                    // Ocultar la barra de progress
+                    progressBar.visibility = View.GONE
+                }
+            })
         }
     }
 }
-
